0f4f4e38f4799736a5e06d41b694490b25412761
SREELAKSHMISUD <121602892+SREELAKSHMISUD@users.noreply.github.com>
2024-07-06 02:02:55 +0530

    Add files via upload

---
+++ F string.py
@@ -0,0 +1,5 @@
+#f strings - we use f strings to combine strings and variables helpful for complex datasets or using many variables 
+score=0
+height=1.8
+iswinning= True
+print(f"your score is {score}, your height is {height}, you are winning is {iswinning}")
---
+++ Life in weeks.py
@@ -0,0 +1,4 @@
+age = input()
+years = 90 -int(age)
+weeks =years * 52
+print(f"You have {weeks} weeks left.")
